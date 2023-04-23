33e725dad1f6c85b91d43f0c1ee575d4c0eb4be6
jrp2212 <72630123+jrp2212@users.noreply.github.com>
2023-04-22 23:18:21 -0400

    Pictures are sorted and added to respective folder

--- canvas.py
+++ canvas.py
@@ -14,12 +14,25 @@ def get_canvas(img_dict, fruit):
     canvas_size = (num_cols * 100, len(img_keys) * 160)
     canvas = np.zeros((canvas_size[1], canvas_size[0], 3), dtype=np.uint8)
 
-    for i, key in enumerate(img_keys):
-        path = img_dict[key]
-        img = cv2.imread(path)
-        img = cv2.resize(img, THUMBNAIL_SIZE)
+    for row in range(8):
+        # Iterate over columns
+        for col in range(num_cols):
+            # Calculate index into list of image keys
+            idx = row * num_cols + col
+            # Load image
+            path = img_dict[img_keys[idx]][0]
+            img = cv2.imread(path)
+            img = cv2.resize(img, THUMBNAIL_SIZE)
+            # Calculate position in canvas to insert image
+            x = col * THUMBNAIL_SIZE[0]
+            y = row * THUMBNAIL_SIZE[1]
+            # Insert image into canvas
+            canvas[y:y+THUMBNAIL_SIZE[1], x:x+THUMBNAIL_SIZE[0], :] = img
 
-        y_offset = i * 160
+    cv2.imshow('Canvas', canvas)
+    cv2.waitKey(0)
+    cv2.destroyAllWindows()
 
-        canvas[y_offset:y_offset+100, 0:0+100, :] = img
+        
+
 
--- main.py
+++ main.py
@@ -1,8 +1,9 @@
 from dict import build_image_dict, get_image_dict
+from canvas import get_canvas
 
 build_image_dict("Day1")
 build_image_dict("Day2")
 
 img_dict = get_image_dict()
 
-print(img_dict.keys())
+get_canvas(img_dict, 'apple')
