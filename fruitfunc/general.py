e783b3cadc01cebd8d8842ea4595493543a28725
mshroff123 <maansishroff@gmail.com>
2023-04-27 01:13:29 -0400

    updates to general

--- fruitfunc/general.py
+++ fruitfunc/general.py
@@ -19,16 +19,36 @@ def get_color_histogram(img_name, r_bits, g_bits, b_bits):
     return hist
 
 """
+HUE AND SATURATION:
+The function uses HSV image to get the hues for an image
+Pretty similar to color image -- minimal difference
+"""
+def get_hue_sat_histogram(img_name, num_bins):
+    query_img = Image.open(img_name)
+    query_hsv = query_img.convert('HSV')
+    query_np = np.array(query_hsv)
+    
+    # extract the hue and saturation channels and flatten
+    hue_channel = query_np[:, :, 0].flatten()
+    sat_channel = query_np[:, :, 1].flatten()
+    
+    # create the 1d histograms
+    hue_hist, _ = np.histogram(hue_channel, bins=num_bins)
+    sat_hist, _ = np.histogram(sat_channel, bins=num_bins)
+    return hue_hist, sat_hist
+
+
+"""
 TEXTURE:
 Reads in the image and converts it grayscale
 Converts grayscale to Laplacian
 Gets histogram of Laplacian numpy array with 2**5 bins
 """
-def get_texture_hist(img_name):
+def get_texture_hist(img_name, num_bins):
     query_img = Image.open(img_name)
     gray_img = query_img.convert('L')
     laplacian_np = np.array(laplace(np.array(gray_img))).reshape(-1,1)
-    hist, _ = np.histogramdd(np.absolute(laplacian_np), bins=2**5)
+    hist, _ = np.histogramdd(np.absolute(laplacian_np), bins=num_bins)
     return hist
 
 # calls the PIL built in function to show a PIL image
@@ -38,5 +58,6 @@ def display_PIL(PIL_img):
 
 if __name__ == "__main__":
     img_name = "../Day1/mango1.JPG"
-    get_texture_hist(img_name, 2**5)
+    get_hue_histogram(img_name, 2**5)
+    # get_texture_hist(img_name, 2**5)
     print("success")
